530564f17638e9a8fb329b8aa76abaf93d0313fb
Z Z <Z@Z.com>
2026-05-14 15:57:38 +0300

    p2petrovich 14052026 apkpure 3011

--- TelegramNewsReader/app/src/main/java/com/p2petrovich/telegramnewsreader/ApiConfig.kt
+++ TelegramNewsReader/app/src/main/java/com/p2petrovich/telegramnewsreader/ApiConfig.kt
@@ -1,7 +1,9 @@
 package com.p2petrovich.telegramnewsreader
 
 object ApiConfig {
+    @Suppress("ConstPropertyName")
     val TELEGRAM_API_ID: Int = BuildConfig.TELEGRAM_API_ID
+    @Suppress("ConstPropertyName")
     val TELEGRAM_API_HASH: String = BuildConfig.TELEGRAM_API_HASH
     const val DATABASE_DIRECTORY = "tdlib"
     const val FILES_DIRECTORY = "files"
