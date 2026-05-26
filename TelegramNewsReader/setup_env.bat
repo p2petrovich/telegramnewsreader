edd2eb99033798834d3f14ec60ff6eb0e45eb121
p2petrovich <p2petrovich@gmail.com>
2026-05-26 19:46:31 +0300

    Delete TelegramNewsReader/setup_env.bat

--- TelegramNewsReader/setup_env.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set JAVA_HOME=C:\Users\Admin\AppData\Local\Programs\Eclipse Adoptium\jdk-17.0.15.6-hotspot
-set PATH=%JAVA_HOME%\bin;%PATH%
-echo Java environment set up
-echo JAVA_HOME=%JAVA_HOME%
-echo PATH=%PATH%
-cmd /k
