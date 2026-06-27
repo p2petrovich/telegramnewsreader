c19515687a75b1f8bb2c26759196bce14ec881c2
p2petrovich <218076514+p2petrovich@users.noreply.github.com>
2026-06-27 12:42:37 +0300

    27062026 таймер

--- TelegramNewsReader/app/src/main/java/com/p2petrovich/telegramnewsreader/utils/DebugConfig.kt
+++ TelegramNewsReader/app/src/main/java/com/p2petrovich/telegramnewsreader/utils/DebugConfig.kt
@@ -6,14 +6,14 @@ package com.p2petrovich.telegramnewsreader.utils
  */
 object DebugConfig {
     /** Логирование этапов обработки новостей (RAW, FILTER, DEDUP и т.д.) */
-    const val LOG_PIPELINE_STAGES = true
+    const val LOG_PIPELINE_STAGES = false
 
     /** Детальные логи запросов в Telegram (пагинация, чаты) */
-    const val LOG_TG_HISTORY = true
+    const val LOG_TG_HISTORY = false
 
     /** Логи дедупликатора (попадания в базу, отсев) */
-    const val LOG_DEDUP_DETAILS = true
+    const val LOG_DEDUP_DETAILS = false
 
     /** Логи событий плеера и пометки новостей как прочитанных */
-    const val LOG_PLAYER_EVENTS = true
+    const val LOG_PLAYER_EVENTS = false
 }
